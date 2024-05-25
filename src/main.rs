419c7ef0b242cabc1de3c32aca0b42efee29e596
Felipe Lee <35938642+felipe-lee@users.noreply.github.com>
2024-05-25 16:39:40 -0700

    feat: enable the ability to play again

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,71 @@ use std::io;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("Welcome, this is the best number guessing game on the block!");
 
     loop {
+        println!("Guess the number! (1-100)");
+
+        let secret_number = rand::thread_rng().gen_range(1..=100);
+
         println!("Please input your guess!");
 
-        let mut guess = String::new();
+        loop {
+            let mut guess = String::new();
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+            io::stdin()
+                .read_line(&mut guess)
+                .expect("Failed to read your guess");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
+            let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Please input a number");
+                    continue;
+                }
+            };
 
-        println!("You guessed: {guess}");
+            println!("You guessed: {guess}");
 
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
+            match guess.cmp(&secret_number) {
+                Ordering::Less => println!("Too small, try again..."),
+                Ordering::Greater => println!("Too big, try again..."),
+                Ordering::Equal => {
+                    println!("You win!");
+                    break;
+                }
             }
+        }
+
+        let mut player_answer = String::new();
+
+        println!("Would you like to play again? (y/n)");
+
+        let mut close_game = false;
+        loop {
+            io::stdin()
+                .read_line(&mut player_answer)
+                .expect("Failed to read your answer");
+
+            match player_answer.as_str().trim() {
+                "y" => {
+                    println!("Great, thanks for sticking around, hope you have fun!");
+                    break;
+                }
+                "n" => {
+                    println!("Thanks for playing, hope to see you again!");
+                    close_game = true;
+                    break;
+                }
+                _ => {
+                    println!("Invalid option, please input y or n");
+                    continue;
+                }
+            }
+        }
+
+        if close_game {
+            break;
         }
     }
 }
