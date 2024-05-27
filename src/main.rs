c08cbf122f1a8bbad3366bc7919ac27390d9e9c6
Felipe Lee <35938642+felipe-lee@users.noreply.github.com>
2024-05-27 14:10:22 -0700

    refactor: update main to use functions for game logic

--- src/main.rs
+++ src/main.rs
@@ -6,69 +6,83 @@ use rand::Rng;
 fn main() {
     println!("Welcome, this is the best number guessing game on the block!");
 
+    run_guessing_game()
+}
+
+fn run_guessing_game() {
     loop {
         println!("Guess the number! (1-100)");
 
-        let secret_number = rand::thread_rng().gen_range(1..=100);
+        play_game();
 
-        println!("Please input your guess!");
+        if !check_if_player_wants_to_continue_playing() {
+            return;
+        }
+    }
+}
 
-        loop {
-            let mut guess = String::new();
+fn play_game() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-            io::stdin()
-                .read_line(&mut guess)
-                .expect("Failed to read your guess");
+    println!("Please input your guess!");
 
-            let guess: u32 = match guess.trim().parse() {
-                Ok(num) => num,
-                Err(_) => {
-                    println!("Please input a number");
-                    continue;
-                }
-            };
+    let mut guess = String::new();
 
-            println!("You guessed: {guess}");
+    loop {
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read your guess");
 
-            match guess.cmp(&secret_number) {
-                Ordering::Less => println!("Too small, try again..."),
-                Ordering::Greater => println!("Too big, try again..."),
-                Ordering::Equal => {
-                    println!("You win!");
-                    break;
-                }
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input a number");
+                continue;
+            }
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => {
+                println!("Too small, try again...");
+                continue;
+            }
+            Ordering::Greater => {
+                println!("Too big, try again...");
+                continue;
+            }
+            Ordering::Equal => {
+                println!("You win!");
+                return;
             }
         }
+    }
+}
 
-        let mut player_answer = String::new();
+fn check_if_player_wants_to_continue_playing() -> bool {
+    let mut player_answer = String::new();
 
-        println!("Would you like to play again? (y/n)");
+    println!("Would you like to play again? (y/n)");
 
-        let mut close_game = false;
-        loop {
-            io::stdin()
-                .read_line(&mut player_answer)
-                .expect("Failed to read your answer");
+    loop {
+        io::stdin()
+            .read_line(&mut player_answer)
+            .expect("Failed to read your answer");
 
-            match player_answer.as_str().trim() {
-                "y" => {
-                    println!("Great, thanks for sticking around, hope you have fun!");
-                    break;
-                }
-                "n" => {
-                    println!("Thanks for playing, hope to see you again!");
-                    close_game = true;
-                    break;
-                }
-                _ => {
-                    println!("Invalid option, please input y or n");
-                    continue;
-                }
+        match player_answer.as_str().trim() {
+            "y" => {
+                println!("Great, thanks for sticking around, hope you have fun!");
+                return true;
             }
-        }
-
-        if close_game {
-            break;
+            "n" => {
+                println!("Thanks for playing, hope to see you again!");
+                return false;
+            }
+            _ => {
+                println!("Invalid option, please input y or n");
+                continue;
+            }
         }
     }
 }
